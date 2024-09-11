c1917e1685e156b7ef65270447a3a60d581fbe29
Ricardo Balam <rbalamcm@gmail.com>
2024-09-11 14:06:37 -0600

    Proyecto java anidados

---
+++ src/TriangleReverse.java
@@ -0,0 +1,22 @@
+
+public class TriangleReverse {
+   public static void main(String[] args) {
+	
+	   
+
+			   
+			   for(int i=6; i>=0; i--) {
+				   
+				   for(int j=0; j<=i ; j++) {
+					   System.out.print("*");
+				   }
+				 System.out.println("");
+			   }
+			   
+	
+		
+
+	   
+   }
+   
+}
